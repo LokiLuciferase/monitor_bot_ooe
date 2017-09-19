c082eef762433b3400486cdf5c2021bb36400b70
LokiLuciferase <luxxus1@gmail.com>
2017-09-19 14:28:38 +0200

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,4 +2,4 @@
 
 # disable power saving mode of wifi
 cat interfaces > /etc/network/interfaces
-echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=1 rtw_ips_mode=1" > /etc/modprobe.d/8192cu.conf
+echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=0 rtw_ips_mode=1" > /etc/modprobe.d/8192cu.conf
