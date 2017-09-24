cf1636db82d1a054097cfdbca6499cb34dbb804f
LokiLuciferase <luxxus1@gmail.com>
2017-09-24 16:40:28 +0200

    Added nightmode and timestamp to camera functions.
    Signed-off-by: LokiLuciferase <luxxus1@gmail.com>

--- lib/timelapse.py
+++ lib/timelapse.py
@@ -11,7 +11,6 @@ hot_time_lapse = False
 endtimes = None
 
 
-# creates a thread which takes images at set intervals and ultimately returns a timelapse video
 class TimelapseThread(threading.Thread):
 
     def __init__(self, snaps_per_h, total_snaps, snaptime, delay):
@@ -51,7 +50,7 @@ def timelapse(snaps_per_h, total_snaps, snaptime, waitfor):
     for times in range(total_snaps):
         try:
             lapse_pic_name = "%s/lapse%s.png" % (lapse_folder_name, str(times + 1).zfill(3))
-            snap(lapse_pic_name, qual='sd')
+            snap(lapse_pic_name, qual='sd', ts=True)
         except:
             pass
         sleep(3600 // boundedsnaps)
