e4afda5c76cb6444a876ca99f676feb9c17658e6
LokiLuciferase <luxxus1@gmail.com>
2017-10-02 21:11:35 +0200

    updated update file to enable ethernet
    Signed-off-by: LokiLuciferase <luxxus1@gmail.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+
+pip install telepot picamera pifacedigitalio pifacecommons
+
 # disable power saving mode of wifi
 cat interfaces > /etc/network/interfaces
 echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=1 rtw_ips_mode=1" > /etc/modprobe.d/8192cu.conf
+
+# disable camera LED
+echo "disable_camera_led=1" >> /boot/config.txt
