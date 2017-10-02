15310b82bbc11c1806f37352ca7d94ffc1952a2a
LokiLuciferase <luxxus1@gmail.com>
2017-10-02 21:30:25 +0200

    updated update file
    Signed-off-by: LokiLuciferase <luxxus1@gmail.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
 
-pip install telepot picamera pifacedigitalio pifacecommons
+# install bot dependencies
+pip3 install telepot picamera pifacedigitalio pifacecommons
 
 # disable power saving mode of wifi
 cat interfaces > /etc/network/interfaces
 echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=1 rtw_ips_mode=1" > /etc/modprobe.d/8192cu.conf
 
+# disable stats LEDs
+echo "dtparam=act_led_trigger=none" >> /boot/config.txt
+echo "dtparam=act_led_activelow=off" >> /boot/config.txt
+
+echo "dtparam=pwr_led_trigger=none" >> /boot/config.txt
+echo "dtparam=pwr_led_activelow=off" >> /boot/config.txt
+
 # disable camera LED
 echo "disable_camera_led=1" >> /boot/config.txt
+
+# overvolt and start X
+echo "start_x=1" >> /boot/config.txt
+echo "over_voltage=2" >> /boot/config.txt
