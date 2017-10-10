115650259c03838f38d150ecc9e6ad99f4005734
LokiLuciferase <luxxus1@gmail.com>
2017-10-11 00:51:44 +0200

    Added script to compile the bot to binary with pyinstaller.

--- setup/build_with_pyinstaller.sh
+++ setup/build_with_pyinstaller.sh
@@ -3,6 +3,8 @@
 # builds the bot to a single binary in the folder /home/pi/scripts/pyi_mbo , then move it back here
 # this path is defined in buildspec where it has to be changed if necessary
 
+cd /home/pi/scripts/monitor_bot_ooe/setup
+
 if [[ $USER != "root" ]]; then
     echo "This script requires root privileges."
     exit 1
@@ -13,28 +15,32 @@ if [[ -n $1 ]]; then
 
         echo "Building bot with minimal (shell commands only) functionality."
         cp -r ../../monitor_bot_ooe/ ../../pyi_mbo
-        cp buildspec ../../pyi_mbo
+        cp buildspec ../../pyi_mbo/build.spec
         pushd ../../pyi_mbo
         sed -i 's/building=False/building=True/g' ./lib/textparser.py
-        pyinstaller buildspec
+        pyinstaller build.spec
         popd
-        mv ../../pyi_mbo/dist .
-        cp -a ../scripts ./dist
-        rm -rf ../../pyi_mbo
-        chown -R $SUDO_USER dist
     fi
 else
 echo "Building bot with full RPi functionality."
 cp -r ../../monitor_bot_ooe/ ../../pyi_mbo
-cp buildspec ../../pyi_mbo
+cp buildspec ../../pyi_mbo/build.spec
 pushd ../../pyi_mbo
-pyinstaller buildspec
+pyinstaller build.spec
 popd
+fi
+
 mv ../../pyi_mbo/dist .
 cp -a ../scripts ./dist
 rm -rf ../../pyi_mbo
 chown -R $SUDO_USER dist
-fi
 
+# write build note
+tokenfile="../data/tokens.py"
+allowed_users=$(grep "valid_users" $tokenfile)
+thistoken=$(grep "TOKEN" $tokenfile)
 
+echo "Token of this bot and its ID on telegram: ${thistoken}
+Telegram nicknames allowed to interact with this bot: ${allowed_users}" > dist/buildnotes.txt
 
+echo "Build has completed."
